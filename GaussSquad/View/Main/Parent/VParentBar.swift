86b982bfdeb099d1d8f538faa2c637d5b998327b
vauxhall <zero.griffin@gmail.com>
2017-01-26 15:49:31 -0600

    button home

--- GaussSquad/View/Main/Parent/VParentBar.swift
+++ GaussSquad/View/Main/Parent/VParentBar.swift
@@ -3,7 +3,11 @@ import UIKit
 class VParentBar:UIView
 {
     private weak var controller:CParent!
+    private weak var buttonHome:VParentBarButton!
+    private weak var layoutHomeLeft:NSLayoutConstraint!
     private let kBorderHeight:CGFloat = 1
+    private let kButtonsTop:CGFloat = 20
+    private let kButtonsWidth:CGFloat = 70
     
     init(controller:CParent)
     {
@@ -14,7 +18,13 @@ class VParentBar:UIView
         
         let border:VBorder = VBorder(color:UIColor(white:0, alpha:0.1))
         
+        let buttonHome:VParentBarButton = VParentBarButton(
+            image:#imageLiteral(resourceName: "assetGenericHome"))
+        buttonHome.isSelected = true
+        self.buttonHome = buttonHome
+        
         addSubview(border)
+        addSubview(buttonHome)
         
         NSLayoutConstraint.height(
             view:border,
@@ -25,10 +35,34 @@ class VParentBar:UIView
         NSLayoutConstraint.equalsHorizontal(
             view:border,
             toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:buttonHome,
+            toView:self,
+            constant:kButtonsTop)
+        NSLayoutConstraint.bottomToBottom(
+            view:buttonHome,
+            toView:self)
+        layoutHomeLeft = NSLayoutConstraint.leftToLeft(
+            view:buttonHome,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:buttonHome,
+            constant:kButtonsWidth)
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.maxX
+        let remainHome:CGFloat = width - kButtonsWidth
+        let marginHome:CGFloat = remainHome / 2.0
+        layoutHomeLeft.constant = marginHome
+        
+        super.layoutSubviews()
     }
 }
--- GaussSquad/View/Main/Parent/VParentBarButton.swift
+++ GaussSquad/View/Main/Parent/VParentBarButton.swift
@@ -2,12 +2,27 @@ import UIKit
 
 class VParentBarButton:UIButton
 {
-    convenience init(image:UIImage)
+    init(image:UIImage)
     {
-        self.init()
+        super.init(frame:CGRect.zero)
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
+        setImage(
+            image.withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.selected)
+        setImage(
+            image.withRenderingMode(UIImageRenderingMode.alwaysOriginal),
+            for:UIControlState.highlighted)
+        setImage(
+            image.withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.normal)
         imageView!.clipsToBounds = true
         imageView!.contentMode = UIViewContentMode.center
+        imageView!.tintColor = UIColor(white:0.9, alpha:1)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
     }
 }
