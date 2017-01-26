aeaeb0a591521ff5d92122b9eb5d3387ec36023c
vauxhall <zero.griffin@gmail.com>
2017-01-26 14:17:56 -0600

    added item arithmetic

--- GaussSquad/Model/Home/MHomeItemArithmetic.swift
+++ GaussSquad/Model/Home/MHomeItemArithmetic.swift
@@ -1,9 +1,6 @@
-//
-//  MHomeItemArithmetic.swift
-//  GaussSquad
-//
-//  Created by zero on 1/26/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MHomeItemLinearArithmetic:MHomeItem
+{
+    
+}
