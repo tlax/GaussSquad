019ae3beaf07e515240bc65c8cecc1e4d3749865
vauxhall <zero.griffin@gmail.com>
2017-01-26 14:56:32 -0600

    view parent bar

--- GaussSquad/View/Home/VHome.swift
+++ GaussSquad/View/Home/VHome.swift
@@ -2,5 +2,16 @@ import UIKit
 
 class VHome:VView
 {
+    private weak var controller:CHome!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CHome
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }
