22b979c35d41f9ec7a78ebb8c283e281a109fc02
vauxhall <zero.griffin@gmail.com>
2017-01-29 15:42:51 -0600

    mutable label gauss

--- GaussSquad/View/LinearEquations/Landing/VLinearEquationsBar.swift
+++ GaussSquad/View/LinearEquations/Landing/VLinearEquationsBar.swift
@@ -3,14 +3,11 @@ import UIKit
 class VLinearEquationsBar:UIView
 {
     private weak var controller:CLinearEquations!
-    private weak var labelTitle:UILabel!
-    private weak var labelGauss:UILabel!
-    private weak var labelInfo:UILabel!
-    private weak var imageGauss:UIImageView!
     private let kContentTop:CGFloat = 20
     private let kBackWidth:CGFloat = 60
     private let kBackHeight:CGFloat = 44
     private let kImageGaussSize:CGFloat = 130
+    private let kLabelGaussRight:CGFloat = -10
     
     init(controller:CLinearEquations)
     {
@@ -19,6 +16,20 @@ class VLinearEquationsBar:UIView
         clipsToBounds = true
         backgroundColor = UIColor.white
         self.controller = controller
+        
+        let stringGaussName:NSAttributedString = NSAttributedString(
+            string:NSLocalizedString("VLinearEquationsBar_labelGaussName", comment:""),
+            attributes:[
+                NSFontAttributeName:UIFont.medium(size:15),
+                NSForegroundColorAttributeName:UIColor.squadBlue])
+        let stringGaussDate:NSAttributedString = NSAttributedString(
+            string:NSLocalizedString("VLinearEquationsBar_labelGaussDate", comment:""),
+            attributes:[
+                NSFontAttributeName:UIFont.regular(size:15),
+                NSForegroundColorAttributeName:UIColor.black])
+        let stringGauss:NSMutableAttributedString = NSMutableAttributedString()
+        stringGauss.append(stringGaussName)
+        stringGauss.append(stringGaussDate)
         
         let buttonBack:UIButton = UIButton()
         buttonBack.translatesAutoresizingMaskIntoConstraints = false
@@ -46,9 +57,22 @@ class VLinearEquationsBar:UIView
         labelTitle.text = NSLocalizedString("VLinearEquationsBar_labelTitle", comment:"")
         
         let imageGauss:UIImageView = UIImageView()
+        imageGauss.isUserInteractionEnabled = false
+        imageGauss.translatesAutoresizingMaskIntoConstraints = false
+        imageGauss.clipsToBounds = true
+        imageGauss.contentMode = UIViewContentMode.center
+        imageGauss.image = #imageLiteral(resourceName: "assetGenericGauss")
         
+        let labelGauss:UILabel = UILabel()
+        labelGauss.isUserInteractionEnabled = false
+        labelGauss.translatesAutoresizingMaskIntoConstraints = false
+        labelGauss.backgroundColor = UIColor.clear
+        labelGauss.numberOfLines = 0
+        labelGauss.attributedText = stringGauss
         
         addSubview(labelTitle)
+        addSubview(labelGauss)
+        addSubview(imageGauss)
         addSubview(buttonBack)
         
         NSLayoutConstraint.topToTop(
@@ -75,6 +99,27 @@ class VLinearEquationsBar:UIView
         NSLayoutConstraint.equalsHorizontal(
             view:labelTitle,
             toView:self)
+        
+        NSLayoutConstraint.topToBottom(
+            view:imageGauss,
+            toView:buttonBack)
+        NSLayoutConstraint.size(
+            view:imageGauss,
+            constant:kImageGaussSize)
+        NSLayoutConstraint.leftToLeft(
+            view:imageGauss,
+            toView:self)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:labelGauss,
+            toView:imageGauss)
+        NSLayoutConstraint.leftToRight(
+            view:labelGauss,
+            toView:imageGauss)
+        NSLayoutConstraint.rightToRight(
+            view:labelGauss,
+            toView:self,
+            constant:kLabelGaussRight)
     }
     
     required init?(coder:NSCoder)
