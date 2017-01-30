aab8e0f50fd074b20b909cb072bced88fcc18091
vauxhall <zero.griffin@gmail.com>
2017-01-30 12:29:22 -0600

    project items factory

---
+++ GaussSquad/Model/LinearEquations/Projects/MLinearEquationsProjectRowItemOperator.swift
@@ -0,0 +1,9 @@
+//
+//  MLinearEquationsProjectRowItemOperator.swift
+//  GaussSquad
+//
+//  Created by zero on 1/30/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
