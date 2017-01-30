3a3bc548fe07a353d5da7c6347602a2495a211a0
vauxhall <zero.griffin@gmail.com>
2017-01-30 17:57:50 -0600

    project items name

--- GaussSquad/View/LinearEquations/Landing/VLinearEquationsCell.swift
+++ GaussSquad/View/LinearEquations/Landing/VLinearEquationsCell.swift
@@ -3,14 +3,34 @@ import UIKit
 class VLinearEquationsCell:UICollectionViewCell
 {
     private weak var model:MLinearEquationsItem?
+    private weak var label:UILabel!
     private let kAlphaSelected:CGFloat = 0.3
     private let kAlphaNotSelected:CGFloat = 1
+    private let kLabelMargin:CGFloat = 10
  
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
-        backgroundColor = UIColor.blue
+        backgroundColor = UIColor.white
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.medium(size:15)
+        label.textColor = UIColor.black
+        self.label = label
+        
+        addSubview(label)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:label,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:label,
+            toView:self,
+            margin:kLabelMargin)
     }
     
     required init?(coder:NSCoder)
@@ -53,6 +73,7 @@ class VLinearEquationsCell:UICollectionViewCell
     func config(model:MLinearEquationsItem)
     {
         self.model = model
+        label.text = model.name
         hover()
     }
 }
