3af0e5d8180f148d2d2c4d5c7051df36624cf1be
vauxhall <zero.griffin@gmail.com>
2017-01-31 15:03:32 -0600

    cell coefficient whole

--- GaussSquad/Model/LinearEquations/Projects/Items/MLinearEquationsProjectRowItemCoefficientWhole.swift
+++ GaussSquad/Model/LinearEquations/Projects/Items/MLinearEquationsProjectRowItemCoefficientWhole.swift
@@ -2,5 +2,39 @@ import UIKit
 
 class MLinearEquationsProjectRowItemCoefficientWhole:MLinearEquationsProjectRowItemCoefficient
 {
+    let attributedString:NSAttributedString
+    private let kFontSize:CGFloat = 20
+    private let kMaxHeight:CGFloat = 30
+    private let kMaxWidth:CGFloat = 5000
+    private let kStringMargin:CGFloat = 20
     
+    init(coefficientDividend:Double, coefficientDivisor:Double, column:Int)
+    {
+        let coefficient:Double = coefficientDividend / coefficientDivisor
+        let reusableIdentifier:String = VLinearEquationsProjectCellCoefficientWhole.reusableIdentifier
+        let drawingOptions:NSStringDrawingOptions = NSStringDrawingOptions([
+            NSStringDrawingOptions.usesFontLeading,
+            NSStringDrawingOptions.usesLineFragmentOrigin])
+        let attributes:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.regular(size:kFontSize)]
+        let rawString:String = "\(coefficient)"
+        attributedString = NSAttributedString(
+            string:rawString,
+            attributes:attributes)
+        
+        let maxSize:CGSize = CGSize(
+            width:kMaxWidth,
+            height:kMaxHeight)
+        let stringRect:CGRect = attributedString.boundingRect(
+            with:maxSize,
+            options:drawingOptions,
+            context:nil)
+        let stringWidth:CGFloat = ceil(stringRect.size.width)
+        let cellWidth:CGFloat = stringWidth + kStringMargin
+        
+        super.init(
+            cellWidth:cellWidth,
+            reusableIdentifier:reusableIdentifier,
+            column:column)
+    }
 }
