39a1b027edb1fdf95ae680774a05d4dfac55f530
vauxhall <zero.griffin@gmail.com>
2017-01-31 13:23:22 -0600

    view cell division

--- GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCellCoefficientDivision.swift
+++ GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCellCoefficientDivision.swift
@@ -1,9 +1,6 @@
-//
-//  VLinearEquationsProjectCellCoefficientDivision.swift
-//  GaussSquad
-//
-//  Created by zero on 1/31/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VLinearEquationsProjectCellCoefficientDivision:VLinearEquationsProjectCell
+{
+    
+}
