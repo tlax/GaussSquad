691d39a82f1a2e3fa1a784abdb7f528d13769abc
vauxhall <zero.griffin@gmail.com>
2017-01-31 14:49:12 -0600

    view cell operator

--- GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCell.swift
+++ GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCell.swift
@@ -2,15 +2,55 @@ import UIKit
 
 class VLinearEquationsProjectCell:UICollectionViewCell
 {
+    private let kAlphaSelected:CGFloat = 0.3
+    private let kAlphaNotSelected:CGFloat = 1
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
-        backgroundColor = UIColor.blue
+        backgroundColor = UIColor.clear
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            alpha = kAlphaSelected
+        }
+        else
+        {
+            alpha = kAlphaNotSelected
+        }
+    }
+    
+    //MARK: public
+    
+    func config(model:MLinearEquationsProjectRowItem)
+    {
+        hover()
     }
 }
