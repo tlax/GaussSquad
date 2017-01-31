0997b1d2db99f10180a183dbffa638d795226ccf
vauxhall <zero.griffin@gmail.com>
2017-01-31 15:40:40 -0600

    cell indeterminate

--- GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCellIndeterminateNone.swift
+++ GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCellIndeterminateNone.swift
@@ -2,5 +2,26 @@ import UIKit
 
 class VLinearEquationsProjectCellIndeterminateNone:VLinearEquationsProjectCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.isUserInteractionEnabled = false
+        imageView.image = #imageLiteral(resourceName: "assetGenericColIndeterminateNone")
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCellIndeterminateSymbol.swift
+++ GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCellIndeterminateSymbol.swift
@@ -2,5 +2,43 @@ import UIKit
 
 class VLinearEquationsProjectCellIndeterminateSymbol:VLinearEquationsProjectCell
 {
+    private weak var label:UILabel!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.textAlignment = NSTextAlignment.center
+        label.textColor = UIColor.black
+        self.label = label
+        
+        NSLayoutConstraint.equals(
+            view:label,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func config(model:MLinearEquationsProjectRowItem)
+    {
+        super.config(model:model)
+        
+        guard
+        
+            let model:MLinearEquationsProjectRowItemIndeterminateSymbol = model as? MLinearEquationsProjectRowItemIndeterminateSymbol
+        
+        else
+        {
+            return
+        }
+        
+        label.attributedText = model.attributedString
+    }
 }
