8f59d2831ad6f55cd9932c68130cbf38892911fb
vauxhall <zero.griffin@gmail.com>
2017-02-01 16:52:57 -0600

    added blur to view polynomial

--- GaussSquad/View/LinearEquations/Polynomial/VLinearEquationsPolynomial.swift
+++ GaussSquad/View/LinearEquations/Polynomial/VLinearEquationsPolynomial.swift
@@ -2,5 +2,25 @@ import UIKit
 
 class VLinearEquationsPolynomial:VView
 {
+    private weak var controller:CLinearEquationsPolynomial!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        self.controller = controller as? CLinearEquationsPolynomial
+        
+        let blur:VBlur = VBlur.light()
+        
+        addSubview(blur)
+        
+        NSLayoutConstraint.equals(
+            view:blur,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
