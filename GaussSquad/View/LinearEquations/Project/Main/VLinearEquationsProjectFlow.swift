2fc32751bc32f334a11969aa92e3f85187de6c28
vauxhall <zero.griffin@gmail.com>
2017-02-01 15:18:02 -0600

    layout attributes for elements in rect

--- GaussSquad/View/LinearEquations/Project/Main/VLinearEquationsProjectFlow.swift
+++ GaussSquad/View/LinearEquations/Project/Main/VLinearEquationsProjectFlow.swift
@@ -3,15 +3,18 @@ import UIKit
 class VLinearEquationsProjectFlow:UICollectionViewFlowLayout
 {
     private weak var model:MLinearEquationsProject!
-    private var layoutAttributes:[UICollectionViewLayoutAttributes]?
+    private var layoutAttributes:[UICollectionViewLayoutAttributes]
     private var contentWidth:CGFloat
     private var contentHeight:CGFloat
+    private let kCellHeight:CGFloat = 80
     
     init(model:MLinearEquationsProject)
     {
         self.model = model
         contentWidth = 0
         contentHeight = 0
+        layoutAttributes = []
+        
         super.init()
     }
     
@@ -24,16 +27,94 @@ class VLinearEquationsProjectFlow:UICollectionViewFlowLayout
     {
         super.prepare()
         
-        if layoutAttributes == nil
+        layoutAttributes = []
+        
+        var section:Int = 0
+        var maxPositionX:CGFloat = 0
+        var positionY:CGFloat = 0
+        
+        for row:MLinearEquationsProjectRow in model.rows
         {
-            print("shit")
-            layoutAttributes = []
+            var item:Int = 0
+            var positionX:CGFloat = 0
+            
+            for col:MLinearEquationsProjectRowItem in row.items
+            {
+                let indexPath:IndexPath = IndexPath(
+                    item:item,
+                    section:section)
+                let colWidth:CGFloat = col.cellWidth
+                let frame:CGRect = CGRect(
+                    x:positionX,
+                    y:positionY,
+                    width:colWidth,
+                    height:kCellHeight)
+                
+                item += 1
+                positionX += colWidth
+                
+                guard
+                    
+                    let attributes:UICollectionViewLayoutAttributes = layoutAttributesForItem(
+                        at:indexPath)
+                
+                else
+                {
+                    continue
+                }
+                
+                attributes.frame = frame
+            }
+            
+            if positionX > maxPositionX
+            {
+                maxPositionX = positionX
+            }
+            
+            section += 1
+            positionY += kCellHeight
+        }
+        
+        contentWidth = maxPositionX
+        contentHeight = positionY
+    }
+    
+    override var collectionViewContentSize:CGSize
+    {
+        get
+        {
+            let size:CGSize = CGSize(
+                width:contentWidth,
+                height:contentHeight)
+            
+            return size
         }
     }
     
-    override func invalidateLayout()
+    override func layoutAttributesForElements(in rect:CGRect) -> [UICollectionViewLayoutAttributes]?
     {
-        super.invalidateLayout()
-//        layoutAttributes = nil
+        var attributes:[UICollectionViewLayoutAttributes]?
+        
+        for layoutAttribute:UICollectionViewLayoutAttributes in layoutAttributes
+        {
+            let frame:CGRect = layoutAttribute.frame
+            
+            if frame.intersects(rect)
+            {
+                if attributes == nil
+                {
+                    attributes = []
+                }
+                
+                attributes!.append(layoutAttribute)
+            }
+        }
+        
+        return attributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds:CGRect) -> Bool
+    {
+        return false
     }
 }
