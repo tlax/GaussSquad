44e57e6e479d0aa152cf427cc64078e000cb7f80
vauxhall <zero.griffin@gmail.com>
2017-02-01 09:03:58 -0600

    cell new polynomial

--- GaussSquad/Model/LinearEquations/Projects/Items/MLinearEquationsProjectRowItemNewPolynomial.swift
+++ GaussSquad/Model/LinearEquations/Projects/Items/MLinearEquationsProjectRowItemNewPolynomial.swift
@@ -2,4 +2,14 @@ import UIKit
 
 class MLinearEquationsProjectRowItemNewPolynomial:MLinearEquationsProjectRowItem
 {
+    private let kCellWidth:CGFloat = 60
+    
+    init(column:Int)
+    {
+        super.init(
+            polynomial:nil,
+            cellWidth:kCellWidth,
+            reusableIdentifier: <#T##String#>,
+            column: <#T##Int#>)
+    }
 }
