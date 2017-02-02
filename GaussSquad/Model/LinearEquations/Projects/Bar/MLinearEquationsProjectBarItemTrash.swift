f5d64f84cc75b90086f6acd9f517dab1c5831efd
vauxhall <zero.griffin@gmail.com>
2017-02-02 14:53:27 -0600

    item trash

---
+++ GaussSquad/Model/LinearEquations/Projects/Bar/MLinearEquationsProjectBarItemTrash.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MLinearEquationsProjectBarItemTrash:MLinearEquationsProjectBarItem
+{
+    init()
+    {
+        super.init(image:#imageLiteral(resourceName: "assetGenericTrash"))
+    }
+    
+    override func selected(controller:CLinearEquationsProject)
+    {
+        controller.save()
+    }
+}
