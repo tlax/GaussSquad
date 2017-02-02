324c85b0494d5ea2dc640db449d76d971affb735
vauxhall <zero.griffin@gmail.com>
2017-02-02 09:27:12 -0600

    cell new row

---
+++ GaussSquad/View/LinearEquations/Project/Items/VLinearEquationsProjectCellNewRow.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class VLinearEquationsProjectCellNewRow:VLinearEquationsProjectCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.isUserInteractionEnabled = false
+        imageView.image = #imageLiteral(resourceName: "assetGenericColNewRow")
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
