133a67e562803b8bd3ad830fbbc3a9ef7a26c468
vauxhall <zero.griffin@gmail.com>
2017-02-02 14:54:16 -0600

    item compress

---
+++ GaussSquad/Model/LinearEquations/Projects/Bar/MLinearEquationsProjectBarItemCompress.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MLinearEquationsProjectBarItemCompress:MLinearEquationsProjectBarItem
+{
+    init()
+    {
+        super.init(image:#imageLiteral(resourceName: "assetGenericCompress"))
+    }
+    
+    override func selected(controller:CLinearEquationsProject)
+    {
+        controller.save()
+    }
+}
