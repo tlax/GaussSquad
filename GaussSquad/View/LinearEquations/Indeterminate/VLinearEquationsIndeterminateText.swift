8e70fe74a685777351b69cd47d1459f70c33e82b
vauxhall <zero.griffin@gmail.com>
2017-02-02 23:32:53 -0600

    indeterminate text

--- GaussSquad/View/LinearEquations/Indeterminate/VLinearEquationsIndeterminateText.swift
+++ GaussSquad/View/LinearEquations/Indeterminate/VLinearEquationsIndeterminateText.swift
@@ -2,5 +2,41 @@ import UIKit
 
 class VlineaEquationsIndeterminateText:UITextField, UITextFieldDelegate
 {
+    private weak var controller:CLinearEquationsIndeterminate!
+    private let kFontSize:CGFloat = 70
     
+    init(controller:CLinearEquationsIndeterminate)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor.clear
+        textColor = UIColor.black
+        tintColor = UIColor.black
+        returnKeyType = UIReturnKeyType.default
+        keyboardAppearance = UIKeyboardAppearance.light
+        autocorrectionType = UITextAutocorrectionType.no
+        spellCheckingType = UITextSpellCheckingType.no
+        autocapitalizationType = UITextAutocapitalizationType.none
+        keyboardType = UIKeyboardType.alphabet
+        borderStyle = UITextBorderStyle.none
+        clearButtonMode = UITextFieldViewMode.never
+        font = UIFont.numeric(size:kFontSize)
+        delegate = self
+        self.controller = controller
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: textField delegate
+    
+    func textFieldShouldReturn(_ textField:UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        
+        return true
+    }
 }
