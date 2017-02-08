19b96d7d5fb6e072615908e57e868321710f6296
vauxhall <zero.griffin@gmail.com>
2017-02-08 17:44:12 -0600

    move to left and to right now in equation

--- GaussSquad/Data/DEquationExtension.swift
+++ GaussSquad/Data/DEquationExtension.swift
@@ -32,4 +32,62 @@ extension DEquation
         removeFromPolynomials(polynomial)
         DManager.sharedInstance?.delete(object:polynomial)
     }
+    
+    func moveToLeft(polynomial:DPolynomial)
+    {
+        guard
+            
+            let equation:DEquation = polynomial.equationResult
+            
+        else
+        {
+            return
+        }
+        
+        if equation == self
+        {
+            DManager.sharedInstance?.createManagedObject(
+                entityName:DPolynomial.entityName)
+            { (created:NSManagedObject?) in
+                
+                guard
+                    
+                    let newPolynomial:DPolynomial = created as? DPolynomial
+                    
+                else
+                {
+                    return
+                }
+                
+                newPolynomial.equationPolynomials = equation
+                newPolynomial.coefficientDividend = polynomial.coefficientDividend
+                newPolynomial.coefficientDivisor = polynomial.coefficientDivisor
+                newPolynomial.isPositive = !polynomial.isPositive
+                newPolynomial.showAsDivision = polynomial.showAsDivision
+                newPolynomial.indeterminate = polynomial.indeterminate
+                
+                equation.restartResult()
+            }
+        }
+    }
+    
+    func moveToRight(polynomial:DPolynomial)
+    {
+        guard
+            
+            let equation:DEquation = polynomial.equationResult,
+            let result:DPolynomial = self.result,
+            let inversed:DPolynomial = polynomial.inversed()
+            
+        else
+        {
+            return
+        }
+        
+        if equation == self
+        {
+            result.add(polynomial:inversed)
+            equation.deletePolynomial(polynomial:polynomial)
+        }
+    }
 }
