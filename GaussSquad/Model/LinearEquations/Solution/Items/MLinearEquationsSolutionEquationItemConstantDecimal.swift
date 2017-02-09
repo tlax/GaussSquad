2fdff9cc5db7baf984308a0e2226052b30b4c2d6
vauxhall <zero.griffin@gmail.com>
2017-02-09 13:29:18 -0600

    constants decimal and division

--- GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemConstantDecimal.swift
+++ GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemConstantDecimal.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class MLinearEquationsSolutionEquationItemConstantDecimal:MLinearEquationsSolutionEquationItemConstant
 {
-    
+    init(
+        coefficientDividend:Double,
+        coefficientDivisor:Double)
+    {
+        let reusableIdentifier:String = VLinearEquationsSolutionCellConstantDecimal.reusableIdentifier
+        let cellWidth:CGFloat = 100
+        
+        super.init(
+            coefficientDividend:coefficientDividend,
+            coefficientDivisor:coefficientDivisor,
+            reusableIdentifier:reusableIdentifier,
+            cellWidth:cellWidth)
+    }
 }
--- GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemConstantDivision.swift
+++ GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemConstantDivision.swift
@@ -2,5 +2,17 @@ import UIKit
 
 class MLinearEquationsSolutionEquationItemConstantDivision:MLinearEquationsSolutionEquationItemConstant
 {
-    
+    init(
+        coefficientDividend:Double,
+        coefficientDivisor:Double)
+    {
+        let reusableIdentifier:String = VLinearEquationsSolutionCellConstantDivision.reusableIdentifier
+        let cellWidth:CGFloat = 100
+        
+        super.init(
+            coefficientDividend:coefficientDividend,
+            coefficientDivisor:coefficientDivisor,
+            reusableIdentifier:reusableIdentifier,
+            cellWidth:cellWidth)
+    }
 }
