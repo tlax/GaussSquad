76ba1cb17c05f523a95a81f51eb74cd5e3aabb09
vauxhall <zero.griffin@gmail.com>
2017-02-09 11:01:14 -0600

    model steps

--- GaussSquad/Model/LinearEquations/Solution/Base/MLinearEquationsSolution.swift
+++ GaussSquad/Model/LinearEquations/Solution/Base/MLinearEquationsSolution.swift
@@ -3,10 +3,12 @@ import Foundation
 class MLinearEquationsSolution
 {
     private weak var project:DProject?
+    private(set) var steps:[MLinearEquationsSolutionStep]
     
     init(project:DProject)
     {
         self.project = project
+        steps = []
     }
     
     //MARK: public
