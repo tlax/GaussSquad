e64ec0a18d4e0b4908acd4dcc945615c359bc01b
vauxhall <zero.griffin@gmail.com>
2017-02-09 10:15:20 -0600

    solution bar

--- GaussSquad/View/LinearEquations/Solution/VLinearEquationsSolution.swift
+++ GaussSquad/View/LinearEquations/Solution/VLinearEquationsSolution.swift
@@ -1,6 +1,17 @@
 import UIKit
 
-class VLinearEquationsSolutions:VView
+class VLinearEquationsSolution:VView
 {
+    private weak var controller:CLinearEquationsSolution!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CLinearEquationsSolution
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
