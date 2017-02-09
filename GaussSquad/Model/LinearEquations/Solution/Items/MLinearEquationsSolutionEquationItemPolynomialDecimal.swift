3e5c37905109fc74373ceddbb1a421c266949d2e
vauxhall <zero.griffin@gmail.com>
2017-02-09 13:24:45 -0600

    model polynomial decimal

--- GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemPolynomialDecimal.swift
+++ GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemPolynomialDecimal.swift
@@ -2,5 +2,19 @@ import UIKit
 
 class MLinearEquationsSolutionEquationItemPolynomialDecimal:MLinearEquationsSolutionEquationItemPolynomial
 {
-    
+    init(
+        indeterminate:DIndeterminate,
+        coefficientDividend:Double,
+        coefficientDivisor:Double)
+    {
+        let reusableIdentifier:String = VLinearEquationsSolutionCellPolynomialDecimal.reusableIdentifier
+        let cellWidth:CGFloat = 100
+        
+        super.init(
+            indeterminate:indeterminate,
+            coefficientDividend:coefficientDividend,
+            coefficientDivisor:coefficientDivisor,
+            reusableIdentifier:reusableIdentifier,
+            cellWidth:cellWidth)
+    }
 }
