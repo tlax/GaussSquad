68b84adc2423238b317c94a76ae90b3fe47280f9
vauxhall <zero.griffin@gmail.com>
2017-02-09 10:57:12 -0600

    model solution step

---
+++ GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemConstantDecimal.swift
@@ -0,0 +1,9 @@
+//
+//  MLinearEquationsSolutionEquationItemConstantDecimal.swift
+//  GaussSquad
+//
+//  Created by zero on 2/9/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemPolynomialDivision.swift
@@ -0,0 +1,9 @@
+//
+//  MLinearEquationsSolutionEquationItemPolynomialDivision.swift
+//  GaussSquad
+//
+//  Created by zero on 2/9/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
