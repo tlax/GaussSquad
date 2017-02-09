657ffe19520fa6df772deef0cd693aa5298f73fc
vauxhall <zero.griffin@gmail.com>
2017-02-09 10:20:35 -0600

    bar constraints

--- GaussSquad/Controller/LinearEquations/CLinearEquationsSolution.swift
+++ GaussSquad/Controller/LinearEquations/CLinearEquationsSolution.swift
@@ -2,5 +2,25 @@ import UIKit
 
 class CLinearEquationsSolution:CController
 {
+    private weak var viewSolution:VLinearEquationsSolution!
     
+    override func loadView()
+    {
+        let viewSolution:VLinearEquationsSolution = VLinearEquationsSolution(
+            controller:self)
+        self.viewSolution = viewSolution
+        view = viewSolution
+    }
+    
+    //MARK: public
+    
+    func back()
+    {
+        parentController.pop(horizontal:CParent.TransitionHorizontal.fromRight)
+    }
+    
+    func plot()
+    {
+        
+    }
 }
--- GaussSquad/View/LinearEquations/Solution/VLinearEquationsSolution.swift
+++ GaussSquad/View/LinearEquations/Solution/VLinearEquationsSolution.swift
@@ -3,15 +3,41 @@ import UIKit
 class VLinearEquationsSolution:VView
 {
     private weak var controller:CLinearEquationsSolution!
+    private weak var viewBar:VLinearEquationsSolutionBar!
+    private weak var layoutBarTop:NSLayoutConstraint!
+    private let kBarHeight:CGFloat = 80
     
     override init(controller:CController)
     {
         super.init(controller:controller)
         self.controller = controller as? CLinearEquationsSolution
+        
+        let viewBar:VLinearEquationsSolutionBar = VLinearEquationsSolutionBar(
+            controller:self.controller)
+        self.viewBar = viewBar
+        
+        addSubview(viewBar)
+        
+        layoutBarTop = NSLayoutConstraint.topToTop(
+            view:viewBar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewBar,
+            constant:kBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewBar,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: public
+    
+    func refresh()
+    {
+        
     }
 }
