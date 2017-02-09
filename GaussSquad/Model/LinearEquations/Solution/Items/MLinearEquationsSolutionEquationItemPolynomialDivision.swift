939dcff74191c1d9df580d6906ee3eb1d9f6d348
vauxhall <zero.griffin@gmail.com>
2017-02-09 13:27:19 -0600

    model polynomial division

--- GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemPolynomialDivision.swift
+++ GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemPolynomialDivision.swift
@@ -2,5 +2,19 @@ import UIKit
 
 class MLinearEquationsSolutionEquationItemPolynomialDivision:MLinearEquationsSolutionEquationItemPolynomial
 {
-    
+    init(
+        indeterminate:DIndeterminate,
+        coefficientDividend:Double,
+        coefficientDivisor:Double)
+    {
+        let reusableIdentifier:String = VLinearEquationsSolutionCellPolynomialDivision.reusableIdentifier
+        let cellWidth:CGFloat = 100
+        
+        super.init(
+            indeterminate:indeterminate,
+            coefficientDividend:coefficientDividend,
+            coefficientDivisor:coefficientDivisor,
+            reusableIdentifier:reusableIdentifier,
+            cellWidth:cellWidth)
+    }
 }
