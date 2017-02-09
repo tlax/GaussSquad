3b9c2addf38a6bafc513255453ce7ce2e7afaf8b
vauxhall <zero.griffin@gmail.com>
2017-02-09 12:37:43 -0600

    step done

--- GaussSquad/Model/LinearEquations/Solution/Steps/MLinearEquationsSolutionStepDone.swift
+++ GaussSquad/Model/LinearEquations/Solution/Steps/MLinearEquationsSolutionStepDone.swift
@@ -1,6 +1,16 @@
-import Foundation
+import UIKit
 
 class MLinearEquationsSolutionStepDone:MLinearEquationsSolutionStep
 {
+    private let kHeaderHeight:CGFloat = 150
     
+    init(equations:[MLinearEquationsSolutionEquation])
+    {
+        let reusableIdentifier:String = VLinearEquationsSolutionHeaderDone.reusableIdentifier
+        
+        super.init(
+            equations:equations,
+            reusableIdentifier:reusableIdentifier,
+            headerHeight:kHeaderHeight)
+    }
 }
