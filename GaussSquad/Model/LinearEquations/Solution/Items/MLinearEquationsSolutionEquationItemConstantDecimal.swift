f4a36abf033e4664d4e04dc95658d99112b13e74
vauxhall <zero.griffin@gmail.com>
2017-02-10 10:49:09 -0600

    model constant decimal

--- GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemConstantDecimal.swift
+++ GaussSquad/Model/LinearEquations/Solution/Items/MLinearEquationsSolutionEquationItemConstantDecimal.swift
@@ -2,13 +2,50 @@ import UIKit
 
 class MLinearEquationsSolutionEquationItemConstantDecimal:MLinearEquationsSolutionEquationItemConstant
 {
+    let string:NSAttributedString
+    private let kFontSize:CGFloat = 20
+    private let kMaxStringWidth:CGFloat = 5000
+    private let kMaxStringHeight:CGFloat = 30
+    private let kAddedWidth:CGFloat = 20
+    
     init(
         coefficientDividend:Double,
         coefficientDivisor:Double,
         showSign:Bool)
     {
+        let attributes:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.numeric(size:kFontSize)]
+        let drawingOptions:NSStringDrawingOptions = NSStringDrawingOptions([
+            NSStringDrawingOptions.usesFontLeading,
+            NSStringDrawingOptions.usesLineFragmentOrigin])
+        let maxSize:CGSize = CGSize(
+            width:kMaxStringWidth,
+            height:kMaxStringHeight)
+        let coefficient:Double = coefficientDividend / coefficientDivisor
+        let absoluteCoefficient:Double = abs(coefficient)
+        let rawString:String = MSession.sharedInstance.stringFrom(
+            number:absoluteCoefficient)
+        
+        string = NSAttributedString(
+            string:rawString,
+            attributes:attributes)
+        
+        let stringRect:CGRect = string.boundingRect(
+            with:maxSize,
+            options:drawingOptions,
+            context:nil)
+        let textWidth:CGFloat = ceil(stringRect.size.width)
         let reusableIdentifier:String = VLinearEquationsSolutionCellConstantDecimal.reusableIdentifier
-        let cellWidth:CGFloat = 100
+        let cellWidth:CGFloat
+        
+        if showSign
+        {
+            cellWidth = textWidth + kAddedWidth
+        }
+        else
+        {
+            cellWidth = textWidth
+        }
         
         super.init(
             coefficientDividend:coefficientDividend,
