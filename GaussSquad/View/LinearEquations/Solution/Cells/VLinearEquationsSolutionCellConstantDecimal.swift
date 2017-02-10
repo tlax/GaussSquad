f53adc43865c774f51c46c64d2c5ce96a329dfb2
vauxhall <zero.griffin@gmail.com>
2017-02-10 10:47:26 -0600

    view cell constant decimal

--- GaussSquad/View/LinearEquations/Solution/Cells/VLinearEquationsSolutionCellConstantDecimal.swift
+++ GaussSquad/View/LinearEquations/Solution/Cells/VLinearEquationsSolutionCellConstantDecimal.swift
@@ -2,5 +2,81 @@ import UIKit
 
 class VLinearEquationsSolutionCellConstantDecimal:VLinearEquationsSolutionCell
 {
+    private weak var label:UILabel!
+    private weak var imageView:UIImageView!
+    private let kImageWidth:CGFloat = 20
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.textAlignment = NSTextAlignment.right
+        label.textColor = UIColor.black
+        self.label = label
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.isUserInteractionEnabled = false
+        self.imageView = imageView
+        
+        addSubview(label)
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.leftToLeft(
+            view:imageView,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:imageView,
+            constant:kImageWidth)
+        
+        NSLayoutConstraint.equals(
+            view:label,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func config(
+        model:MLinearEquationsSolutionEquationItem,
+        index:IndexPath)
+    {
+        guard
+            
+            let model:MLinearEquationsSolutionEquationItemConstantDecimal = model as? MLinearEquationsSolutionEquationItemConstantDecimal
+            
+        else
+        {
+            return
+        }
+        
+        label.attributedText = model.string
+        
+        if model.showSign
+        {
+            if model.coefficientDividend >= 0
+            {
+                imageView.image = #imageLiteral(resourceName: "assetGenericColAddSmall")
+            }
+            else
+            {
+                imageView.image = #imageLiteral(resourceName: "assetGenericColSubstractSmall")
+            }
+        }
+        else
+        {
+            imageView.image = nil
+        }
+    }
 }
