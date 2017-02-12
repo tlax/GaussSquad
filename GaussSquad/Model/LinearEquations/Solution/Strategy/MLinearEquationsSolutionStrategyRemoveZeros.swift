40bffd3a90659ada50ed2787f8cf69557f1f84ae
vauxhall <zero.griffin@gmail.com>
2017-02-11 21:08:45 -0600

    removing zero

--- GaussSquad/Model/LinearEquations/Solution/Strategy/MLinearEquationsSolutionStrategyRemoveZeros.swift
+++ GaussSquad/Model/LinearEquations/Solution/Strategy/MLinearEquationsSolutionStrategyRemoveZeros.swift
@@ -12,7 +12,32 @@ class MLinearEquationsSolutionStrategyRemoveZeros:MLinearEquationsSolutionStrate
             
             for indexPolynomial:Int in 0 ..< countPolynomials
             {
+                let item:MLinearEquationsSolutionEquationItem = equation.items[indexPolynomial]
                 
+                if let polynomial:MLinearEquationsSolutionEquationItemPolynomial = item as? MLinearEquationsSolutionEquationItemPolynomial
+                {
+                    if polynomial.coefficientDividend == 0
+                    {
+                        let strategy:MLinearEquationsSolutionStrategyRemoveZeros = MLinearEquationsSolutionStrategyRemoveZeros(
+                            step:step,
+                            indexEquation:indexEquation,
+                            indexPolynomial:indexPolynomial)
+                        
+                        return strategy
+                    }
+                }
+                else if let coefficient:MLinearEquationsSolutionEquationItemConstant = item as? MLinearEquationsSolutionEquationItemConstant
+                {
+                    if coefficient.coefficientDividend == 0
+                    {
+                        let strategy:MLinearEquationsSolutionStrategyRemoveZeros = MLinearEquationsSolutionStrategyRemoveZeros(
+                            step:step,
+                            indexEquation:indexEquation,
+                            indexPolynomial:indexPolynomial)
+                        
+                        return strategy
+                    }
+                }
             }
             
             indexEquation += 1
@@ -24,7 +49,7 @@ class MLinearEquationsSolutionStrategyRemoveZeros:MLinearEquationsSolutionStrate
     let indexEquation:Int
     let indexPolynomial:Int
     
-    init(
+    private init(
         step:MLinearEquationsSolutionStep,
         indexEquation:Int,
         indexPolynomial:Int)
@@ -45,5 +70,49 @@ class MLinearEquationsSolutionStrategyRemoveZeros:MLinearEquationsSolutionStrate
     
     private func removeZero()
     {
+        var equations:[MLinearEquationsSolutionEquation] = []
+        let descr:String = String(
+            format:NSLocalizedString("MLinearEquationsSolutionStrategyRemoveZeros_descr", comment:""),
+            "\((indexEquation + 1))")
+        
+        let countEquations:Int = self.step.equations.count
+        
+        for indexEquation:Int in 0 ..< countEquations
+        {
+            let equation:MLinearEquationsSolutionEquation
+            let currentEquation:MLinearEquationsSolutionEquation = self.step.equations[indexEquation]
+            
+            if indexEquation == self.indexEquation
+            {
+                var items:[MLinearEquationsSolutionEquationItem] = []
+                let result:MLinearEquationsSolutionEquationItem = currentEquation.result
+                let countItems:Int = currentEquation.items.count
+                
+                for indexItem:Int in 0 ..< countItems
+                {
+                    if indexItem != indexPolynomial
+                    {
+                        let currentItem:MLinearEquationsSolutionEquationItem = currentEquation.items[indexItem]
+                        items.append(currentItem)
+                    }
+                }
+                
+                equation = MLinearEquationsSolutionEquation(
+                    items:items,
+                    result:result,
+                    equationIndex:indexEquation)
+            }
+            else
+            {
+                equation = currentEquation
+            }
+            
+            equations.append(equation)
+        }
+        
+        let step:MLinearEquationsSolutionStepProcess = MLinearEquationsSolutionStepProcess(
+            equations:equations,
+            descr:descr)
+        completed(step:step)
     }
 }
