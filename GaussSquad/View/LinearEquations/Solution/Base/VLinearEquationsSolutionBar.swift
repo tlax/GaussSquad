37009f7d8f0192c3a5ae2f1a3ea93f1af50d67d2
vauxhall <zero.griffin@gmail.com>
2017-02-13 15:57:47 -0600

    added collection view to solution bar

--- GaussSquad/View/LinearEquations/Solution/Base/VLinearEquationsSolutionBar.swift
+++ GaussSquad/View/LinearEquations/Solution/Base/VLinearEquationsSolutionBar.swift
@@ -1,13 +1,15 @@
 import UIKit
 
-class VLinearEquationsSolutionBar:UIView
+class VLinearEquationsSolutionBar:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private let model:MLinearEquationsSolutionBar
     private weak var controller:CLinearEquationsSolution!
+    private weak var collectionView:VCollection!
     private let kBorderHeight:CGFloat = 1
     private let kBackTop:CGFloat = 20
     private let kBackWidth:CGFloat = 60
     private let kBackHeight:CGFloat = 44
+    private let kCellSize:CGFloat = 60
     
     init(controller:CLinearEquationsSolution)
     {
@@ -37,7 +39,26 @@ class VLinearEquationsSolutionBar:UIView
             action:#selector(actionBack(sender:)),
             for:UIControlEvents.touchUpInside)
         
+        let collectionView:VCollection = VCollection()
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        self.collectionView = collectionView
+        
+        if let flow:UICollectionViewFlowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+            flow.itemSize = CGSize(width:kCellSize, height:kCellSize)
+            flow.sectionInset = UIEdgeInsets(
+                top:0,
+                left:kBackWidth,
+                bottom:0,
+                right:0)
+        }
+        
         addSubview(border)
+        addSubview(collectionView)
         addSubview(buttonBack)
         
         NSLayoutConstraint.bottomToBottom(
@@ -63,6 +84,16 @@ class VLinearEquationsSolutionBar:UIView
         NSLayoutConstraint.width(
             view:buttonBack,
             constant:kBackWidth)
+        
+        NSLayoutConstraint.height(
+            view:collectionView,
+            constant:kCellSize)
+        NSLayoutConstraint.bottomToBottom(
+            view:collectionView,
+            toView:self)
+        NSLayoutConstraint.equalsHorizontal(
+            view:collectionView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
