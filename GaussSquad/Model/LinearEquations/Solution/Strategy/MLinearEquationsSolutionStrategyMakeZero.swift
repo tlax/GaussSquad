7b689c6cc8ed63c98ae643cd8ba9bd2a900d8444
vauxhall <zero.griffin@gmail.com>
2017-02-13 09:48:15 -0600

    look for equations with only zeros

---
+++ GaussSquad/Model/LinearEquations/Solution/Strategy/MLinearEquationsSolutionStrategyMakeZero.swift
@@ -0,0 +1,136 @@
+import Foundation
+
+class MLinearEquationsSolutionStrategyMakeZero:MLinearEquationsSolutionStrategy
+{
+    class func onlyZeros(step:MLinearEquationsSolutionStep) -> MLinearEquationsSolutionStrategyMakeZero?
+    {
+        var indexEquation:Int = 0
+        
+        for equation:MLinearEquationsSolutionEquation in step.equations
+        {
+            var onlyZeros:Bool = true
+            
+            for item:MLinearEquationsSolutionEquationItem in equation.items
+            {
+                guard
+                
+                    let polynomial:MLinearEquationsSolutionEquationItemPolynomial = item as? MLinearEquationsSolutionEquationItemPolynomial
+                
+                else
+                {
+                    continue
+                }
+                
+                if polynomial.coefficientDividend != 0
+                {
+                    onlyZeros = false
+                    
+                    break
+                }
+            }
+            
+            if onlyZeros
+            {
+                guard
+                
+                    let result:MLinearEquationsSolutionEquationItemConstant = equation.result as? MLinearEquationsSolutionEquationItemConstant
+                
+                else
+                {
+                    return nil
+                }
+                
+                if result.coefficientDividend != 0
+                {
+                    let strategy:MLinearEquationsSolutionStrategyMakeZero = MLinearEquationsSolutionStrategyMakeZero(
+                        step:step,
+                        indexEquation:indexEquation)
+                    
+                    return strategy
+                }
+            }
+            
+            indexEquation += 1
+        }
+        
+        return nil
+    }
+    
+    let indexEquation:Int
+    
+    private init(
+        step:MLinearEquationsSolutionStep,
+        indexEquation:Int)
+    {
+        self.indexEquation = indexEquation
+        super.init(step:step)
+    }
+    
+    override func process(delegate:MLinearEquationsSolutionStrategyDelegate)
+    {
+        super.process(delegate:delegate)
+        
+        removeZero()
+    }
+    
+    //MARK: private
+    
+    private func removeZero()
+    {
+        var equations:[MLinearEquationsSolutionEquation] = []
+        let descr:String = String(
+            format:NSLocalizedString("MLinearEquationsSolutionStrategyRemoveZeros_descr", comment:""),
+            "\((indexEquation + 1))")
+        
+        let countEquations:Int = self.step.equations.count
+        
+        for indexEquation:Int in 0 ..< countEquations
+        {
+            let equation:MLinearEquationsSolutionEquation?
+            let currentEquation:MLinearEquationsSolutionEquation = self.step.equations[indexEquation]
+            
+            if indexEquation == self.indexEquation
+            {
+                let countItems:Int = currentEquation.items.count
+                
+                if countItems > 1
+                {
+                    var items:[MLinearEquationsSolutionEquationItem] = []
+                    let result:MLinearEquationsSolutionEquationItem = currentEquation.result
+                    
+                    for indexItem:Int in 0 ..< countItems
+                    {
+                        if indexItem != indexPolynomial
+                        {
+                            let currentItem:MLinearEquationsSolutionEquationItem = currentEquation.items[indexItem]
+                            items.append(currentItem)
+                        }
+                    }
+                    
+                    equation = MLinearEquationsSolutionEquation(
+                        items:items,
+                        result:result,
+                        equationIndex:indexEquation)
+                }
+                else
+                {
+                    equation = nil
+                }
+            }
+            else
+            {
+                equation = currentEquation
+            }
+            
+            if let equation:MLinearEquationsSolutionEquation = equation
+            {
+                equations.append(equation)
+            }
+        }
+        
+        let step:MLinearEquationsSolutionStepProcess = MLinearEquationsSolutionStepProcess(
+            equations:equations,
+            descr:descr)
+        completed(step:step)
+    }
+}
