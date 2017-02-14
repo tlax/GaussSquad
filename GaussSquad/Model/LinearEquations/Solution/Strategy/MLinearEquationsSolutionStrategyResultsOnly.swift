8bcb0679267c84fd46b772120c7f62bf1e80a2ba
vauxhall <zero.griffin@gmail.com>
2017-02-13 18:00:38 -0600

    strategy show results

--- GaussSquad/Model/LinearEquations/Solution/Strategy/MLinearEquationsSolutionStrategyResultsOnly.swift
+++ GaussSquad/Model/LinearEquations/Solution/Strategy/MLinearEquationsSolutionStrategyResultsOnly.swift
@@ -4,50 +4,49 @@ class MLinearEquationsSolutionStrategyResultsOnly:MLinearEquationsSolutionStrate
 {
     class func finished(step:MLinearEquationsSolutionStep) -> MLinearEquationsSolutionStrategyResultsOnly?
     {
-        var indexEquation:Int = 0
-        
         for equation:MLinearEquationsSolutionEquation in step.equations
         {
-            let pivotIndex:Int = equation.pivotIndex()
-            
             guard
                 
-                let pivotItem:MLinearEquationsSolutionEquationItemPolynomial = equation.items[pivotIndex] as? MLinearEquationsSolutionEquationItemPolynomial
-                
-                else
+                let polynomials:[MLinearEquationsSolutionEquationItemPolynomial] = equation.items as? [MLinearEquationsSolutionEquationItemPolynomial],
+                let _:MLinearEquationsSolutionEquationItemConstant = equation.result as? MLinearEquationsSolutionEquationItemConstant
+            
+            else
             {
-                continue
+                return nil
             }
             
-            let coefficient:Double = pivotItem.coefficient
+            let pivotIndex:Int = equation.pivotIndex()
+            let pivotItem:MLinearEquationsSolutionEquationItemPolynomial = polynomials[pivotIndex]
+            let pivotCoefficient:Double = pivotItem.coefficient
             
-            if coefficient != 1
+            if pivotCoefficient != 1
             {
-                let scalar:Double = 1 / coefficient
-                let strategy:MLinearEquationsSolutionStrategyRemoveCoefficients = MLinearEquationsSolutionStrategyRemoveCoefficients(
-                    step:step,
-                    indexRow:indexEquation,
-                    scalar:scalar)
-                
-                return strategy
+                return nil
             }
             
-            indexEquation += 1
+            for polynomial:MLinearEquationsSolutionEquationItemPolynomial in polynomials
+            {
+                if polynomial !== pivotItem
+                {
+                    let polynomialCoefficient:Double = polynomial.coefficient
+                    
+                    if abs(polynomialCoefficient) > MSession.sharedInstance.kMinNumber
+                    {
+                        return nil
+                    }
+                }
+            }
         }
         
-        return nil
+        let strategy:MLinearEquationsSolutionStrategyResultsOnly = MLinearEquationsSolutionStrategyResultsOnly(
+            step:step)
+        
+        return strategy
     }
     
-    private let indexRow:Int
-    private let scalar:Double
-    
-    private init(
-        step:MLinearEquationsSolutionStep,
-        indexRow:Int,
-        scalar:Double)
+    private init(step:MLinearEquationsSolutionStep)
     {
-        self.indexRow = indexRow
-        self.scalar = scalar
         super.init(step:step)
     }
     
@@ -55,44 +54,47 @@ class MLinearEquationsSolutionStrategyResultsOnly:MLinearEquationsSolutionStrate
     {
         super.process(delegate:delegate)
         
-        removeCoefficient()
+        showResults()
     }
     
     //MARK: private
     
-    private func removeCoefficient()
+    private func showResults()
     {
         var equations:[MLinearEquationsSolutionEquation] = []
-        let scalarString:String = MSession.sharedInstance.stringFrom(number:scalar)
-        let descr:String = String(
-            format:NSLocalizedString("MLinearEquationsSolutionStrategyRemoveCoefficients_descr", comment:""),
-            "\(indexRow + 1)",
-            scalarString)
-        
         var indexEquation:Int = 0
         
         for equation:MLinearEquationsSolutionEquation in self.step.equations
         {
-            let newEquation:MLinearEquationsSolutionEquation
+            guard
+                
+                let polynomials:[MLinearEquationsSolutionEquationItemPolynomial] = equation.items as? [MLinearEquationsSolutionEquationItemPolynomial],
+                let result:MLinearEquationsSolutionEquationItemConstant = equation.result as? MLinearEquationsSolutionEquationItemConstant
             
-            if indexEquation == indexRow
-            {
-                newEquation = equation.multiplyScalar(
-                    scalar:scalar)
-            }
             else
             {
-                newEquation = equation
+                continue
             }
             
-            equations.append(newEquation)
+            let pivotIndex:Int = equation.pivotIndex()
+            let pivotItem:MLinearEquationsSolutionEquationItemPolynomial = polynomials[pivotIndex]
+            let finalItem:MLinearEquationsSolutionEquationItemPolynomial = pivotItem.reIndexed(
+                newIndex:0)
+            let newItems:[MLinearEquationsSolutionEquationItem] = [
+                finalItem
+            ]
             
+            let newEquation:MLinearEquationsSolutionEquation = MLinearEquationsSolutionEquation(
+                items:newItems,
+                result:result,
+                equationIndex:indexEquation)
+            
+            equations.append(newEquation)
             indexEquation += 1
         }
         
-        let step:MLinearEquationsSolutionStepProcess = MLinearEquationsSolutionStepProcess(
-            equations:equations,
-            descr:descr)
+        let step:MLinearEquationsSolutionStepDone = MLinearEquationsSolutionStepDone(
+            equations:equations)
         completed(step:step)
     }
 }
