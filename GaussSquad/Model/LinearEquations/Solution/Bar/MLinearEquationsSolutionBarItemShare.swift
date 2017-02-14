1558b98ebd96f1414ecc10bf32033055584b40d9
vauxhall <zero.griffin@gmail.com>
2017-02-14 15:00:42 -0600

    bar item share

---
+++ GaussSquad/Model/LinearEquations/Solution/Bar/MLinearEquationsSolutionBarItemShare.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MLinearEquationsSolutionBarItemShare:MLinearEquationsSolutionBarItem
+{
+    init()
+    {
+        super.init(image:#imageLiteral(resourceName: "assetGenericSolutionShare"))
+    }
+    
+    override func selected(controller:CLinearEquationsSolution)
+    {
+        controller.bottom()
+    }
+}
