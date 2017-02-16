55cee5dcd488ec362cf91840214108002a91ecdb
vauxhall <zero.griffin@gmail.com>
2017-02-15 18:12:23 -0600

    added models to factory help

--- GaussSquad/Model/Help/LinearEquations/Project/MHelpLinearEquationsProject.swift
+++ GaussSquad/Model/Help/LinearEquations/Project/MHelpLinearEquationsProject.swift
@@ -5,9 +5,19 @@ class MHelpLinearEquationsProject:MHelp
     init()
     {
         let itemPolynomials:MHelpLinearEquationsProjectItemPolynomials = MHelpLinearEquationsProjectItemPolynomials()
+        let itemAddPolynomial:MHelpLinearEquationsProjectItemAddPolynomial = MHelpLinearEquationsProjectItemAddPolynomial()
+        let itemAddRow:MHelpLinearEquationsProjectItemAddRow = MHelpLinearEquationsProjectItemAddRow()
+        let itemIndeterminates:MHelpLinearEquationsProjectItemIndeterminates = MHelpLinearEquationsProjectItemIndeterminates()
+        let itemCompress:MHelpLinearEquationsProjectItemCompress = MHelpLinearEquationsProjectItemCompress()
+        let itemSolution:MHelpLinearEquationsProjectItemSolution = MHelpLinearEquationsProjectItemSolution()
         
         let items:[MHelpItem] = [
-            itemPolynomials
+            itemPolynomials,
+            itemAddPolynomial,
+            itemAddRow,
+            itemIndeterminates,
+            itemCompress,
+            itemSolution
         ]
         
         super.init(items:items)
