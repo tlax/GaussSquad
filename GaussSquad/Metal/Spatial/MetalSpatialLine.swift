50c52b001cc45171801cee5be7b75754aa538110
vauxhall <zero.griffin@gmail.com>
2017-02-17 09:56:56 -0600

    line model

--- GaussSquad/Metal/Spatial/MetalSpatialLine.swift
+++ GaussSquad/Metal/Spatial/MetalSpatialLine.swift
@@ -19,35 +19,33 @@ class MetalSpatialLine
         lineWidth:Float)
     {
         let lineWidth_2:Float = lineWidth / 2.0
-        let aPointXMin:Float
-        let aPointYMin:Float
-        let aPointXMax:Float
-        let aPointYMax:Float
-        let bPointXMin:Float
-        let bPointYMin:Float
-        let bPointXMax:Float
-        let bPointYMax:Float
+        let aPointXMin:Float = aPointX - lineWidth_2
+        let aPointYMin:Float = aPointY - lineWidth_2
+        let aPointXMax:Float = aPointX + lineWidth_2
+        let aPointYMax:Float = aPointY + lineWidth_2
+        let bPointXMin:Float = bPointX - lineWidth_2
+        let bPointYMin:Float = bPointY - lineWidth_2
+        let bPointXMax:Float = bPointX + lineWidth_2
+        let bPointYMax:Float = bPointY + lineWidth_2
         
-        
-        
-        let topLeft:MetalVertex = MetalVertex(
-            positionX:left,
-            positionY:top)
-        let topRight:MetalVertex = MetalVertex(
-            positionX:right,
-            positionY:top)
-        let bottomLeft:MetalVertex = MetalVertex(
-            positionX:left,
-            positionY:bottom)
-        let bottomRight:MetalVertex = MetalVertex(
-            positionX:right,
-            positionY:bottom)
+        let pointAMin:MetalVertex = MetalVertex(
+            positionX:aPointXMin,
+            positionY:aPointYMin)
+        let pointAMax:MetalVertex = MetalVertex(
+            positionX:aPointXMax,
+            positionY:aPointYMax)
+        let pointBMin:MetalVertex = MetalVertex(
+            positionX:bPointXMin,
+            positionY:bPointYMin)
+        let pointBMax:MetalVertex = MetalVertex(
+            positionX:bPointXMax,
+            positionY:bPointYMax)
         
         let vertexFace:MetalVertexFace = MetalVertexFace(
-            topLeft:topLeft,
-            topRight:topRight,
-            bottomLeft:bottomLeft,
-            bottomRight:bottomRight)
+            topLeft:pointAMin,
+            topRight:pointAMax,
+            bottomLeft:pointBMin,
+            bottomRight:pointBMax)
         
         vertexBuffer = device.generateBuffer(bufferable:vertexFace)
     }
