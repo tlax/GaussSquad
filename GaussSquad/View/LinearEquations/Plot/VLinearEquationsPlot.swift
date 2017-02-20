ed288e08d7f2cd8b3934c0562916c2093102e6a2
vauxhall <zero.griffin@gmail.com>
2017-02-19 18:09:40 -0600

    validating touches

--- GaussSquad/View/LinearEquations/Plot/VLinearEquationsPlot.swift
+++ GaussSquad/View/LinearEquations/Plot/VLinearEquationsPlot.swift
@@ -52,19 +52,32 @@ class VLinearEquationsPlot:VView
         return nil
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    override func touchesBegan(_ touches:Set<UITouch>, with event:UIEvent?)
+    {
+        guard
+        
+            let view:UIView = touches.first?.view
+        
+        else
+        {
+            return
+        }
+        
+        if view !== viewBar
+        {
+            
+        }
+    }
+    
+    override func touchesEnded(_ touches:Set<UITouch>, with event:UIEvent?)
     {
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    override func touchesMoved(_ touches:Set<UITouch>, with event:UIEvent?)
     {
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+    override func touchesCancelled(_ touches:Set<UITouch>, with event:UIEvent?)
     {
         
     }
