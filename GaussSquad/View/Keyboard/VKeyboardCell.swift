cd5a2778d33bd4db1edfb6170513fa0b84d04467
vauxhall <zero.griffin@gmail.com>
2017-02-22 17:46:09 -0600

    cell config with model

--- GaussSquad/View/Keyboard/VKeyboardCell.swift
+++ GaussSquad/View/Keyboard/VKeyboardCell.swift
@@ -2,5 +2,37 @@ import UIKit
 
 class VKeyboardCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = UIViewContentMode.center
+        imageView.clipsToBounds = true
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        NSLayoutConstraint.equals(
+            view:imageView,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config(model:MKeyboardRowItem)
+    {
+        imageView.image = model.icon
+    }
 }
