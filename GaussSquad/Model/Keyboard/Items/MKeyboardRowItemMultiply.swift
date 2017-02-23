f32b9c465f90be710c2730098c08b2e602cb9a1e
vauxhall <zero.griffin@gmail.com>
2017-02-23 17:09:03 -0600

    multiply

--- GaussSquad/Model/Keyboard/Items/MKeyboardRowItemMultiply.swift
+++ GaussSquad/Model/Keyboard/Items/MKeyboardRowItemMultiply.swift
@@ -6,4 +6,17 @@ class MKeyboardRowItemMultiply:MKeyboardRowItem
     {
         super.init(icon:#imageLiteral(resourceName: "assetKeyboardMultiply"))
     }
+    
+    override func selected(model:MKeyboard, view:UITextView)
+    {
+        model.states.last?.commitState(
+            model:model,
+            view:view)
+        
+        let previousValue:Double = model.lastNumber()
+        let stateMultiply:MKeyboardStateMultiply = MKeyboardStateMultiply(
+            previousValue:previousValue,
+            editing:model.kInitial)
+        model.states.append(stateMultiply)
+    }
 }
