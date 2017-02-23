7fa028e5fe30074d2593b5385e42a8016a694f38
vauxhall <zero.griffin@gmail.com>
2017-02-23 17:37:43 -0600

    state percent

--- GaussSquad/Model/Keyboard/Items/MKeyboardRowItemPercent.swift
+++ GaussSquad/Model/Keyboard/Items/MKeyboardRowItemPercent.swift
@@ -6,4 +6,15 @@ class MKeyboardRowItemPercent:MKeyboardRowItem
     {
         super.init(icon:#imageLiteral(resourceName: "assetKeyboardPercent"))
     }
+    
+    override func selected(model:MKeyboard, view:UITextView)
+    {
+        commitIfNeeded(model:model, view:view)
+        
+        let previousValue:Double = model.lastNumber()
+        let stateMultiply:MKeyboardStateMultiply = MKeyboardStateMultiply(
+            previousValue:previousValue,
+            editing:model.kInitial)
+        model.states.append(stateMultiply)
+    }
 }
