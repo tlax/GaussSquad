32b247767508165d27df6663a0f1266888e6fa7b
vauxhall <zero.griffin@gmail.com>
2017-02-23 17:58:17 -0600

    model calculator functions

--- GaussSquad/View/Calculator/VCalculatorHistory.swift
+++ GaussSquad/View/Calculator/VCalculatorHistory.swift
@@ -2,5 +2,18 @@ import UIKit
 
 class VCalculatorHistory:UIView
 {
+    private weak var controller:CCalculator!
     
+    init(controller:CCalculator)
+    {
+        super.init(frame:CGRect.zero)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
--- GaussSquad/View/Calculator/VCalculatorHistoryCell.swift
+++ GaussSquad/View/Calculator/VCalculatorHistoryCell.swift
@@ -2,5 +2,23 @@ import UIKit
 
 class VCalculatorHistoryCell:UICollectionViewCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        isUserInteractionEnabled = false
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: public
+    
+    func config()
+    {
+        
+    }
 }
