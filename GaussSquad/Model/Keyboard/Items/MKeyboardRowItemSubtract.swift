600cf50c6bf38726e4fa8b2a3a4e158f54903017
vauxhall <zero.griffin@gmail.com>
2017-02-23 16:12:30 -0600

    subtract selected

--- GaussSquad/Model/Keyboard/Items/MKeyboardRowItemSubtract.swift
+++ GaussSquad/Model/Keyboard/Items/MKeyboardRowItemSubtract.swift
@@ -6,4 +6,17 @@ class MKeyboardRowItemSubtract:MKeyboardRowItem
     {
         super.init(icon:#imageLiteral(resourceName: "assetKeyboardSubtract"))
     }
+    
+    override func selected(model:MKeyboard, view:UITextView)
+    {
+        model.states.last?.commitState(
+            model:model,
+            view:view)
+        
+        let previousValue:Double = model.lastNumber()
+        let stateSubtract:MKeyboardStateSubtract = MKeyboardStateSubtract(
+            previousValue:previousValue,
+            editing:model.kInitial)
+        model.states.append(stateSubtract)
+    }
 }
