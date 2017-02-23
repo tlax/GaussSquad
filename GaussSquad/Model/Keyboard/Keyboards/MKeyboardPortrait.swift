87d5958b6d7f32181d7dd4269d77cfa7ba573f56
vauxhall <zero.griffin@gmail.com>
2017-02-22 18:23:18 -0600

    numeric rows

--- GaussSquad/Model/Keyboard/Keyboards/MKeyboardPortrait.swift
+++ GaussSquad/Model/Keyboard/Keyboards/MKeyboardPortrait.swift
@@ -7,22 +7,43 @@ class MKeyboardPortrait:MKeyboard
         let item0:MKeyboardRowItem0 = MKeyboardRowItem0()
         let item1:MKeyboardRowItem1 = MKeyboardRowItem1()
         let item2:MKeyboardRowItem2 = MKeyboardRowItem2()
+        let item3:MKeyboardRowItem3 = MKeyboardRowItem3()
+        let item4:MKeyboardRowItem4 = MKeyboardRowItem4()
+        let item5:MKeyboardRowItem5 = MKeyboardRowItem5()
+        let item6:MKeyboardRowItem6 = MKeyboardRowItem6()
+        let item7:MKeyboardRowItem7 = MKeyboardRowItem7()
+        let item8:MKeyboardRowItem8 = MKeyboardRowItem8()
+        let item9:MKeyboardRowItem9 = MKeyboardRowItem9()
         
         let itemsFirstRow:[MKeyboardRowItem] = [
-            item0,
-            item1,
-            item2]
+            item7,
+            item8,
+            item9]
         let itemsSecondRow:[MKeyboardRowItem] = [
-            item1]
+            item4,
+            item5,
+            item6]
+        let itemsThirdRow:[MKeyboardRowItem] = [
+            item1,
+            item2,
+            item3]
+        let itemsFourthRow:[MKeyboardRowItem] = [
+            item0]
         
         let firstRow:MKeyboardRow = MKeyboardRow(
             items:itemsFirstRow)
         let secondRow:MKeyboardRow = MKeyboardRow(
             items:itemsSecondRow)
+        let thirdRow:MKeyboardRow = MKeyboardRow(
+            items:itemsThirdRow)
+        let fourthRow:MKeyboardRow = MKeyboardRow(
+            items:itemsFourthRow)
         
         let rows:[MKeyboardRow] = [
             firstRow,
-            secondRow]
+            secondRow,
+            thirdRow,
+            fourthRow]
         
         super.init(rows:rows)
     }
