4e32dbda3ee8ed79e7d5068eaaccbb355e674f0e
vauxhall <zero.griffin@gmail.com>
2017-02-24 16:09:34 -0600

    deg sin

--- GaussSquad/Model/Keyboard/StatesExtra/MKeyboardStateSinDeg.swift
+++ GaussSquad/Model/Keyboard/StatesExtra/MKeyboardStateSinDeg.swift
@@ -4,7 +4,7 @@ class MKeyboardStateSinDeg:MKeyboardStateExtra
 {
     override func unflowedCommitState(model:MKeyboard, view:UITextView)
     {
-        let sinValue:Double = sin(currentValue) * 180 / M_PI
+        let sinValue:Double = sin(currentValue * M_PI / 180.0)
         editing = model.numberAsString(scalar:sinValue)
         
         view.text = model.kEmpty
