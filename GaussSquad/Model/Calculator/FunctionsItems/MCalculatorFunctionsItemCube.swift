ee2426385ac92ecabe852530fb0b0d953e3c8d9d
vauxhall <zero.griffin@gmail.com>
2017-02-25 02:54:35 -0600

    reciprocal and cube

--- GaussSquad/Model/Calculator/FunctionsItems/MCalculatorFunctionsItemCube.swift
+++ GaussSquad/Model/Calculator/FunctionsItems/MCalculatorFunctionsItemCube.swift
@@ -1,9 +1,33 @@
-//
-//  MCalculatorFunctionsItemCube.swift
-//  GaussSquad
-//
-//  Created by zero on 2/25/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MCalculatorFunctionsItemCube:MCalculatorFunctionsItem
+{
+    private let kExponent:Double = 3
+    
+    init()
+    {
+        let icon:UIImage = #imageLiteral(resourceName: "assetFunctionSquareRoot")
+        let title:String = NSLocalizedString("MCalculatorFunctionsItemCube_title", comment:"")
+        
+        super.init(
+            icon:icon,
+            title:title)
+    }
+    
+    override func processFunction(
+        currentValue:Double,
+        currentString:String,
+        modelKeyboard:MKeyboard,
+        view:UITextView)
+    {
+        let cbValue:Double = pow(currentValue, kExponent)
+        let cbString:String = modelKeyboard.numberAsString(scalar:cbValue)
+        let descr:String = "cube (\(currentString)) = \(cbString)"
+        
+        applyUpdate(
+            modelKeyboard:modelKeyboard,
+            view:view,
+            newEditing:cbString,
+            descr:descr)
+    }
+}
--- GaussSquad/Model/Calculator/FunctionsItems/MCalculatorFunctionsItemReciprocal.swift
+++ GaussSquad/Model/Calculator/FunctionsItems/MCalculatorFunctionsItemReciprocal.swift
@@ -1,9 +1,33 @@
-//
-//  MCalculatorFunctionsItemReciprocal.swift
-//  GaussSquad
-//
-//  Created by zero on 2/25/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MCalculatorFunctionsItemReciprocal:MCalculatorFunctionsItem
+{
+    private let kExponent:Double = -1
+    
+    init()
+    {
+        let icon:UIImage = #imageLiteral(resourceName: "assetFunctionSquareRoot")
+        let title:String = NSLocalizedString("MCalculatorFunctionsItemReciprocal_title", comment:"")
+        
+        super.init(
+            icon:icon,
+            title:title)
+    }
+    
+    override func processFunction(
+        currentValue:Double,
+        currentString:String,
+        modelKeyboard:MKeyboard,
+        view:UITextView)
+    {
+        let inversedValue:Double = pow(currentValue, kExponent)
+        let inversedString:String = modelKeyboard.numberAsString(scalar:inversedValue)
+        let descr:String = "reciprocal (\(currentString)) = \(inversedString)"
+        
+        applyUpdate(
+            modelKeyboard:modelKeyboard,
+            view:view,
+            newEditing:inversedString,
+            descr:descr)
+    }
+}
