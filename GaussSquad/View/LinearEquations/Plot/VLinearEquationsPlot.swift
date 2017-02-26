616de7fab9883ff54c7c09db04df72d5b96dba6b
vauxhall <zero.griffin@gmail.com>
2017-02-25 20:35:43 -0600

    refactor plot views

--- GaussSquad/View/LinearEquations/Plot/VLinearEquationsPlot.swift
+++ GaussSquad/View/LinearEquations/Plot/VLinearEquationsPlot.swift
@@ -9,7 +9,7 @@ class VLinearEquationsPlot:VView
     private weak var spinner:VSpinner?
     private var startingPoint:CGPoint?
     private let kBarHeight:CGFloat = 64
-    private let kMenuHeight:CGFloat = 46
+    private let kMenuHeight:CGFloat = 52
     private var positionX:CGFloat = 0
     private var positionY:CGFloat = 0
     
