2cf697c2bb3de4cc3a337586ac7ce10619538bd4
vauxhall <zero.griffin@gmail.com>
2017-02-25 20:10:02 -0600

    model item any root

---
+++ GaussSquad/Model/Calculator/FunctionsItems/MCalculatorFunctionsItemAnyRoot.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+class MCalculatorFunctionsItemAnyRoot:MCalculatorFunctionsItem
+{
+    init()
+    {
+        let icon:UIImage = #imageLiteral(resourceName: "assetFunctionAnyRoot")
+        let title:String = NSLocalizedString("MCalculatorFunctionsItemAnyRoot_title", comment:"")
+        
+        super.init(
+            icon:icon,
+            title:title)
+    }
+    
+    override func processFunction(
+        currentValue:Double,
+        currentString:String,
+        modelKeyboard:MKeyboard,
+        view:UITextView)
+    {
+        let cbrtValue:Double = cbrt(currentValue)
+        let cbrtString:String = modelKeyboard.numberAsString(scalar:cbrtValue)
+        let descr:String = "cube root(\(currentString)) = \(cbrtString)"
+        
+        applyUpdate(
+            modelKeyboard:modelKeyboard,
+            view:view,
+            newEditing:cbrtString,
+            descr:descr)
+    }
+}
