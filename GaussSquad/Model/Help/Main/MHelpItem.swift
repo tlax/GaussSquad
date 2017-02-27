37c5ae896396cdd6ea9726311efa048e2349d8e1
vauxhall <zero.griffin@gmail.com>
2017-02-27 16:18:20 -0600

    added reusable identifier to help item

--- GaussSquad/Model/Help/Main/MHelpItem.swift
+++ GaussSquad/Model/Help/Main/MHelpItem.swift
@@ -2,11 +2,13 @@ import UIKit
 
 class MHelpItem
 {
+    let reusableIdentifier:String
     let image:UIImage
     let title:String
     
-    init(image:UIImage, title:String)
+    init(reusableIdentifier:String, image:UIImage, title:String)
     {
+        self.reusableIdentifier = reusableIdentifier
         self.image = image
         self.title = title
     }
