df5d159eb4f0471ab3f19ce184b2012dcdebf927
vauxhall <zero.griffin@gmail.com>
2017-02-28 11:56:09 -0600

    help item gauss

---
+++ GaussSquad/Model/Help/LinearEquations/Landing/MHelpLinearEquationsLandingItemAddEquation.swift
@@ -0,0 +1,9 @@
+//
+//  MHelpLinearEquationsLandingItemAddEquation.swift
+//  GaussSquad
+//
+//  Created by zero on 2/28/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
