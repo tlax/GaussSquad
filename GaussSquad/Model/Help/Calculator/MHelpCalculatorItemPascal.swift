437513317a37f829ead0af39713d4f6ad8a67176
vauxhall <zero.griffin@gmail.com>
2017-02-27 18:05:57 -0600

    wording

--- GaussSquad/Model/Help/Calculator/MHelpCalculatorItemPascal.swift
+++ GaussSquad/Model/Help/Calculator/MHelpCalculatorItemPascal.swift
@@ -1,9 +1,29 @@
-//
-//  MHelpCalculatorItemPascal.swift
-//  GaussSquad
-//
-//  Created by zero on 2/27/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MHelpCalculatorItemPascal:MHelpItemLong
+{
+    init()
+    {
+        let image:UIImage = #imageLiteral(resourceName: "assetGenericGauss")
+        let attributesTitle:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.bold(size:16),
+            NSForegroundColorAttributeName:UIColor.black]
+        let attributesSubtitle:[String:AnyObject] = [
+            NSFontAttributeName:UIFont.regular(size:16),
+            NSForegroundColorAttributeName:UIColor.black]
+        
+        let rawTitle:String = NSLocalizedString("MHelpCalculatorItemPascal_title", comment:"")
+        let rawSubtitle:String = NSLocalizedString("MHelpCalculatorItemPascal_subtitle", comment:"")
+        
+        let stringTitle:NSAttributedString = NSAttributedString(
+            string:,
+            attributes:attributesTitle)
+        
+        let mutableString:NSMutableAttributedString = NSMutableAttributedString()
+        mutableString.append(<#T##attrString: NSAttributedString##NSAttributedString#>)
+        
+        super.init(
+            image:image,
+            title:mutableString)
+    }
+}
