545dc4179388419a913db894d75b7135832f5c78
vauxhall <zero.griffin@gmail.com>
2017-03-01 15:02:12 -0600

    view ocr with spinner

--- GaussSquad/View/ScannerOCR/VScannerOCR.swift
+++ GaussSquad/View/ScannerOCR/VScannerOCR.swift
@@ -2,5 +2,26 @@ import UIKit
 
 class VScannerOCR:VView
 {
+    private weak var controller:CScannerOCR!
+    private weak var spinner:VSpinner!
     
+    override init(controller:CController)
+    {
+        super.init(controller:controller)
+        self.controller = controller as? CScannerOCR
+        
+        let spinner:VSpinner = VSpinner()
+        self.spinner = spinner
+        
+        addSubview(spinner)
+        
+        NSLayoutConstraint.equals(
+            view:spinner,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
