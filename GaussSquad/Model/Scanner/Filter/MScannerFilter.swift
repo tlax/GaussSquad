3718160691fea44b1751faa30725f1d14a42e3f8
vauxhall <zero.griffin@gmail.com>
2017-03-02 14:47:30 -0600

    export texture and images

--- GaussSquad/Model/Scanner/Filter/MScannerFilter.swift
+++ GaussSquad/Model/Scanner/Filter/MScannerFilter.swift
@@ -3,17 +3,83 @@ import MetalKit
 
 class MScannerFilter
 {
-    private let items:[MScannerFilterItem]
+    private let items:[MScannerFilterItem.Type]
     
     init()
     {
-        items = []
+        let itemHighContrast:MScannerFilterItemHighContrast.Type = MScannerFilterItemHighContrast.self
+        
+        items = [
+            itemHighContrast]
+    }
+    
+    //MARK: private
+    
+    func texturize(
+        textureLoader:MTKTextureLoader,
+        image:UIImage) -> MTLTexture?
+    {
+        let texture:MTLTexture?
+        
+        guard
+            
+            let cgImage:CGImage = image.cgImage
+            
+        else
+        {
+            return nil
+        }
+        
+        do
+        {
+            texture = try textureLoader.newTexture(
+                with:cgImage,
+                options:[
+                    MTKTextureLoaderOptionTextureUsage:
+                        MTLTextureUsage.shaderRead.rawValue as NSObject,
+                    MTKTextureLoaderOptionSRGB:
+                        true as NSObject
+                ])
+        }
+        catch
+        {
+            texture = nil
+        }
+        
+        return texture
     }
     
     //MARK: public
     
     func filter(device:MTLDevice, image:UIImage) -> UIImage?
     {
-        return nil
+        let textureLoader:MTKTextureLoader = MTKTextureLoader(
+            device:device)
+        
+        guard
+        
+            let originalTexture:MTLTexture = texturize(
+                textureLoader:textureLoader,
+                image:image),
+            let library:MTLLibrary = device.newDefaultLibrary()
+        
+        else
+        {
+            return nil
+        }
+        
+        var exportTexture:MTLTexture = originalTexture
+        
+        for item:MScannerFilterItem.Type in items
+        {
+            exportTexture = item.filterTexture(
+                device:device,
+                library:library,
+                texture:exportTexture)
+        }
+        
+        let exportImage:UIImage? = exportTexture.exportImage()
+        
+        return exportImage
     }
 }
