634bfc306fd581e2937217d7989b69afd36b4321
vauxhall <zero.griffin@gmail.com>
2017-03-03 12:14:31 -0600

    crop item

--- GaussSquad/Model/Help/Scanner/MHelpScanner.swift
+++ GaussSquad/Model/Help/Scanner/MHelpScanner.swift
@@ -5,9 +5,11 @@ class MHelpScanner:MHelp
     init()
     {
         let itemTuring:MHelpScannerItemTuring = MHelpScannerItemTuring()
+        let itemCrop:MHelpScannerItemCrop = MHelpScannerItemCrop()
         
         let items:[MHelpItem] = [
-            itemTuring
+            itemTuring,
+            itemCrop
         ]
         
         super.init(items:items)
