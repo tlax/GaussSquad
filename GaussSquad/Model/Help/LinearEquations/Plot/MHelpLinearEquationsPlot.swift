0e9ceb78430d4ee2ff0d76b9a778afb7f2034464
vauxhall <zero.griffin@gmail.com>
2017-03-09 10:43:28 -0600

    model help plot

---
+++ GaussSquad/Model/Help/LinearEquations/Plot/MHelpLinearEquationsPlot.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+class MHelpLinearEquationsPlot:MHelp
+{
+    init()
+    {
+        let itemPolynomials:MHelpLinearEquationsProjectItemPolynomials = MHelpLinearEquationsProjectItemPolynomials()
+        let itemAddPolynomial:MHelpLinearEquationsProjectItemAddPolynomial = MHelpLinearEquationsProjectItemAddPolynomial()
+        let itemEditRow:MHelpLinearEquationsProjectItemEditRow = MHelpLinearEquationsProjectItemEditRow()
+        let itemAddRow:MHelpLinearEquationsProjectItemAddRow = MHelpLinearEquationsProjectItemAddRow()
+        let itemIndeterminates:MHelpLinearEquationsProjectItemIndeterminates = MHelpLinearEquationsProjectItemIndeterminates()
+        let itemCompress:MHelpLinearEquationsProjectItemCompress = MHelpLinearEquationsProjectItemCompress()
+        let itemSolution:MHelpLinearEquationsProjectItemSolution = MHelpLinearEquationsProjectItemSolution()
+        
+        let items:[MHelpItem] = [
+            itemPolynomials,
+            itemAddPolynomial,
+            itemEditRow,
+            itemAddRow,
+            itemIndeterminates,
+            itemCompress,
+            itemSolution
+        ]
+        
+        super.init(items:items)
+    }
+}
---
+++ GaussSquad/Model/Help/LinearEquations/Plot/MHelpLinearEquationsPlotItemCenter.swift
@@ -0,0 +1,9 @@
+//
+//  MHelpLinearEquationsPlotItemCenter.swift
+//  GaussSquad
+//
+//  Created by zero on 3/9/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
